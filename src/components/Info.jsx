cbb47c97d4b1a118a0ee0ae42ff38ccc32d0ffe4
Muhammad Rizki <muhammadrizuki80@gmail.com>
2023-07-22 11:30:32 +0800

    c

--- src/components/Info.jsx
+++ src/components/Info.jsx
@@ -6,16 +6,16 @@ const Info = () => {
     const [t, i18n] = useTranslation('global');      
 
   return (
-    <div className='max-w-[1240px] mx-auto grid lg:grid-cols-5 gap-4 px-4 md:px-10 py-16'>
-        <div className='lg:col-span-3 flex flex-col justify-evenly'>
+    <div className='max-w-[1240px] mx-auto grid lg:grid-cols-3 gap-4 md:px-10 py-16'>
+        <div className='lg:col-span-2 flex justify-evenly'>
             <div>
-                <h2>{t('info.header')}</h2>
-                <p className='text-justify'>
+                <h2 className='px-4'>{t('info.header')}</h2>
+                <p className='flex text-justify px-4'>
                     {t('info.body')}
                 </p>
             </div>
         </div>
-        <div className='lg:col-span-2 flex justify-center items-center'>
+        <div className='flex justify-center items-center'>
             <div className='p-10'>
                 <iframe 
                 src="https://www.youtube.com/embed/wKRL7vkWMoc"
